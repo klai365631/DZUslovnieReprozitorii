ed9ac82ae95bd7e295dfd297e5507a0fea3199ac
ANDRE <klai1@mail.ru>
2022-07-30 23:42:31 +0400

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -8,12 +8,14 @@ public class Main {
             System.out.println("Установите версию приложения для iOS по ссылке");}
         else if(clientOS==1){System.out.println("Установите версию приложения для Android по ссылке");}
 
-        //Задание 2
-        int clientDeviceYear=2015;
-        int iosNew=0;
-        int androidNew=1;
-        Boolean (clientDeviceYear<2016 && 0){System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
-        else (clientDeviceYear<2016 && 1){System.out.println("Установите облегченную версию приложения для Android по ссылке");}
+
+
+
+
+
+
+
+
 
 
 
