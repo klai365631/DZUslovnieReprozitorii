47c46c30dbf9980a1e10a8f1e05c9fca7b073b65
ANDRE <klai1@mail.ru>
2022-07-31 00:11:14 +0400

    Сделал второе задание

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,24 @@
 public class Main {
     public static void main(String[] args) {
         //Задание 1
-        int clientOS=1;
+
+        int clientOS=0;
         int ios=0;
         int android=1;
         if(clientOS==0){
             System.out.println("Установите версию приложения для iOS по ссылке");}
-        else if(clientOS==1){System.out.println("Установите версию приложения для Android по ссылке");}
+         if(clientOS==1){System.out.println("Установите версию приложения для Android по ссылке");}
 
+
+         //Задание 2
+        int clientDeviceYear=2013;
+        int clientOSS=1;
+        int iosF=0;
+        int androidF=1;
+        if(clientDeviceYear<2015&&clientOSS==0){System.out.println("Установите облегченную версию приложения для iOS по ссылке");}
+         if(clientDeviceYear<2015&&clientOSS==1){System.out.println("Установите облегченную версию приложения для Android по ссылке");}
+         if(clientDeviceYear>=2015&&clientOSS==0){System.out.println("Установите версию приложения для iOS по ссылке");}
+        if(clientDeviceYear>=2015&&clientOSS==1){System.out.println("Установите версию приложения для Android по ссылке");}
 
 
 
